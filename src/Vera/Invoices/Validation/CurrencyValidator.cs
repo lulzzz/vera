9f88bbb6557f113c28dc1c82e08aac1dc1a0809a
andreiardel <73880775+andreiardel@users.noreply.github.com>
2021-06-08 11:36:45 +0300

    feat(invoice): currency code validation
    closes issue #76

---
+++ src/Vera/Invoices/Validation/CurrencyValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Vera.Models;
+
+namespace Vera.Invoices.Validation
+{
+    public class CurrencyValidator : IInvoiceValidator
+    {
+        public IEnumerable<ValidationResult> Validate(Invoice invoice)
+        {
+            if (!Currencies.CodeToCurrencyMapper.ContainsKey(invoice.CurrencyCode))
+            {
+                yield return new ValidationResult(
+                    $"The currency {invoice.CurrencyCode} is not valid"
+                );
+            }
+        }
+    }
+}
